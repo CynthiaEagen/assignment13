a6e88801e0bd8de71fa7f981357a551c97b7db83
cynthia.eagen <eagen.cynthia@gmail.com>
2025-04-16 14:54:53 -0400

    Added main game loop. Disabled difficulty buttons. Enabled try again and submit buttons. Added score tracker funcitonality. Left space for presenting the end state.
    Also changed quiz.html slightly to make retrieving the score display <p> more sensible.

--- script.js
+++ script.js
@@ -4,8 +4,16 @@ const question = document.getElementById("question")
 const beginnerButton = document.getElementById("beginnerButton")
 const intermediateButton = document.getElementById("intermediateButton")
 const advancedButton = document.getElementById("advancedButton")
+const tryAgainButton = document.getElementById("tryAgainButton")
+const submitButton = document.getElementById("submitButton")
+const inputBox = document.getElementById('answer')
+const dialogue = document.getElementById('dialogue')
+const scoreDisplay = document.getElementById('score')
+
 var answer = 0
-var operators = [{
+var score = 0
+var round = 1
+const operators = [{
     sign: "+",
     method: function(a,b){return a + b}
 },{
@@ -20,6 +28,15 @@ var operators = [{
 }
 ]
 
+function enterQuizMode() {
+    beginnerButton.disabled = true
+    intermediateButton.disabled = true
+    advancedButton.disabled = true
+    tryAgainButton.disabled = false
+    submitButton.disabled = false
+    quizHandler()
+}
+
 function quizHandler () {
     let newQuestion = generateQuestion(difficulties[difficulty])
     question.innerHTML = newQuestion
@@ -29,19 +46,36 @@ function generateQuestion (range){
     let num1 = Math.floor(Math.random() * (range[1] - range[0])) + range[0]
     let num2 = Math.floor(Math.random() * (range[1] - range[0])) + range[0]
     let op = operators[Math.floor(Math.random() * (4))]
-    console.log(op)
     answer = op.method(num1, num2)
-    console.log(answer)
     return String(num1 + op.sign + num2)
+}
+
+function submitHandler() {
+    if (round > 9) {
+        // present the end state here
+        console.log("end reached")
+        return
+    }
+    dialogue.innerHTML = ""
+    let input = parseFloat(inputBox.value)
+    if (isNaN(input)) {
+        dialogue.innerHTML = "Please enter a number"
+        return
+    }
+    if (input === answer) {
+        score++
+        scoreDisplay.innerText = score + "/10"
+    }
+    round++
+    quizHandler()
 }
 
 function setDifficulty (e) {
     difficulty = e.target.value
+    enterQuizMode()
 }
 
 beginnerButton.addEventListener("click", setDifficulty)
-beginnerButton.addEventListener("click", quizHandler)
 intermediateButton.addEventListener("click", setDifficulty)
-intermediateButton.addEventListener("click", quizHandler)
 advancedButton.addEventListener("click", setDifficulty)
-advancedButton.addEventListener("click", quizHandler)
+submitButton.addEventListener("click", submitHandler)
